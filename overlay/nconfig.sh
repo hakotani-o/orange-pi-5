c581c87c0d8262d4de00f8a297265e8ca9654b11
hakotani-o <139629056+hakotani-o@users.noreply.github.com>
2025-04-09 13:39:52 +0900

    Update nconfig.sh

--- overlay/nconfig.sh
+++ overlay/nconfig.sh
@@ -3,15 +3,6 @@
 rm -f .config
 cat arch/arm64/configs/defconfig ../../overlay/my-add.txt > .config
 
-{
-	sleep 10
-	echo '6'
-	sleep 3
-	echo "\n"
-	sleep 3
-	echo "\n"
-	echo '9'
-	echo "\n"
-} | make nconfig
+
 
 	
